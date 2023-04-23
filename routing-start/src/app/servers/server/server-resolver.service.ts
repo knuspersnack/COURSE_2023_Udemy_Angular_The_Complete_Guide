f07b8ddf2c2fc66412b7a2d748c307c60c8e560d
knuspersnack <thumos@posteo.gr>
2023-04-23 11:21:39 +0200

    Section 11 - Routing

---
+++ routing-start/src/app/servers/server/server-resolver.service.ts
@@ -0,0 +1,30 @@
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { ServersService } from '../servers.service';
+import { Injectable } from '@angular/core';
+
+interface Server {
+  id: number;
+  name: string;
+  status: string;
+}
+
+@Injectable()
+export class ServerResolver implements Resolve<Server> {
+  constructor(private serversService: ServersService) {}
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | { id: number; name: any; string: any; status: string }
+    | Observable<Server>
+    | Promise<Server>
+    | Server {
+    return this.serversService.getServer(route.params['id']);
+  }
+}
